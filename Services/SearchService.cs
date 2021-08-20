00a8318c9639be86144924d683074d521f5460f2
BelleCode <Belinda.tiberio@gmail.com>
2021-08-19 22:24:40 -0700

    Added Search Service

--- Services/SearchService.cs
+++ Services/SearchService.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebBlog.Data;
+using WebBlog.Models;
 
 namespace WebBlog.Services
 {
@@ -10,5 +13,31 @@ namespace WebBlog.Services
         private readonly ApplicationDbContext _context;
 
         public SearchService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Post> ContentSearch(string searchStr)
+        {
+            var posts = _context.Posts.Where(p => p.ReadyStatus == Enums.ReadyStatus.ProductionReady);
+
+            if (!string.IsNullOrEmpty(searchStr))
+            {
+                //Make the search string lowercase
+                searchStr = searchStr.ToLower();
+
+                posts = posts.Where(p =>
+                   p.Title.ToLower().Contains(searchStr) ||
+                   p.Abstract.ToLower().Contains(searchStr) ||
+                   p.Content.ToLower().Contains(searchStr) ||
+                   p.Comments.Any(c =>
+                        c.Body.ToLower().Contains(searchStr) ||
+                        c.ModeratedBody.ToLower().Contains(searchStr) ||
+                        c.BlogUser.FirstName.ToLower().Contains(searchStr) ||
+                        c.BlogUser.LastName.ToLower().Contains(searchStr) ||
+                        c.BlogUser.Email.ToLower().Contains(searchStr)));
+            }
+            return posts.OrderByDescending(p => p.Created);
+        }
     }
 }
