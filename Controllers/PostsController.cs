ab7ee073fe4b08d4c9dfc652057d8a8b3f1d0e90
BelleCode <Belinda.tiberio@gmail.com>
2021-08-22 15:13:19 -0700

    Comments Part 2- LIsting

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -116,9 +116,10 @@ namespace WebBlog.Controllers
 
             var post = await _context.Posts
                 .Include(p => p.Blog)
+                .Include(p => p.BlogUser)
+                .Include(p => p.Tags)
                 .Include(p => p.Comments.Where(p => p.Deleted == null))
                 .ThenInclude(c => c.BlogUser)
-                .Include(p => p.Tags)
                 .FirstOrDefaultAsync(p => p.Slug == slug);
 
             if (post == null)
